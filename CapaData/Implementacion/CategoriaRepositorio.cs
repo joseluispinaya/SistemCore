5209eeec5abd05d7d12d2cb8632e01bfdba5f14c
zerobyte <joseluisdelta1@gmail.com>
2025-11-14 04:08:29 -0400

    inicio proyecto

---
+++ CapaData/Implementacion/CategoriaRepositorio.cs
@@ -0,0 +1,51 @@
+﻿using CapaData.Configuracion;
+using CapaData.Interfaaces;
+using CapaEntidades;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaData.Implementacion
+{
+    public class CategoriaRepositorio : ICategoriaRepositorio
+    {
+        private readonly ConnectionStrings con;
+
+        public CategoriaRepositorio(IOptions<ConnectionStrings> options)
+        {
+            con = options.Value;
+        }
+
+        public async Task<List<Categoria>> Lista()
+        {
+            //List<Categoria> lista = [];
+            List<Categoria> lista = [];
+
+            using (var conexion = new SqlConnection(con.CadenaSQL))
+            {
+                await conexion.OpenAsync();
+                SqlCommand cmd = new("usp_listaCategoria", conexion)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                using var dr = await cmd.ExecuteReaderAsync();
+                while (await dr.ReadAsync())
+                {
+                    lista.Add(new Categoria()
+                    {
+                        IdCategoria = Convert.ToInt32(dr["IdCategoria"]),
+                        Nombre = dr["Nombre"].ToString()!,
+                        Activo = Convert.ToBoolean(dr["Activo"])
+                    });
+                }
+            }
+            return lista;
+        }
+    }
+}
