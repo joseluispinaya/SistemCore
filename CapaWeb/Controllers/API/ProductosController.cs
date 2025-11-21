ce5c0f8d49df65f567697b6a1dd0faed1e793922
zerobyte <joseluisdelta1@gmail.com>
2025-11-21 03:43:57 -0400

    api en controlador web

---
+++ CapaWeb/Controllers/API/ProductosController.cs
@@ -0,0 +1,26 @@
+﻿using CapaData.Interfaaces;
+using CapaEntidades.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapaWeb.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly IProductoRepositorio _repositorio;
+
+        public ProductosController(IProductoRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<ProductoListDTO> lista = await _repositorio.ListaProducDto();
+            return Ok(lista);
+        }
+
+    }
+}
